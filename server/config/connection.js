bd115d2c8f5994eb527fbe2a60beac5c51e3ae3f
Stephanie Carr <sacarr91@gmail.com>
2024-07-26 19:00:00 -0400

    connection code with env var

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
