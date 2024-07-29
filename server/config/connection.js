48cfc95323a57550063769e41ee6a61242be74cf
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-29 19:33:53 -0400

    Moved Joes changes up stream

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,26 +1,7 @@
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require("mongoose");
 
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/dropInDB");
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// Export connection
+module.exports = mongoose.connection;
---
+++ server/models/Sponsor.js
@@ -0,0 +1,2 @@
+// We could think about keeping Sponsorships in a subdocument if it's easier for queries
+// Schema: name, company or organization, sponsorship(another model)
