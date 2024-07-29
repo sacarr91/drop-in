8ec398a659d3e843eb9a83d1cfbdcfe7ec9f833a
Stephanie Carr <sacarr91@gmail.com>
2024-07-29 18:51:52 -0400

    rough draft main.jasx (+ update html for div id=root)

---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './utils/style.css';
+
+
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+
+import Home from './pages/Home';
+import AboutSkater from './pages/AboutSkater';
+import AboutSponsor from './pages/AboutSponsor';
+import Connect from './pages/Connect';
+import SponsorLevels from './pages/SponsorLevels';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/AboutSkater',
+        element: <AboutSkater />,
+      },
+      {
+        path: '/AboutSponsor',
+        element: <AboutSponsor />,
+      },
+      {
+        path: '/Connect',
+        element: <Connect />,
+      },
+      {
+        path: '/SponsorLevels',
+        element: <SponsorLevels />,
+      }
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
