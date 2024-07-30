0c45e88d4946e1bfd2bca0c7ba68c9788814b989
Joseph Ruff <joegruff16@gmail.com>
2024-07-29 20:43:48 -0400

    Created Awards model.

---
+++ server/models/Goals.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const goalSchema = new Schema({
+  name: String,
+  // deadline or date of goal
+});
+
+// Exporting Goals Model to make available
+const Goals = model("goals", goalSchema);
+modoule.exports = Goals;
