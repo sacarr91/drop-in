f1c3f5c1338ed7541b455cf25c895f82a40e1519
Joseph Ruff <joegruff16@gmail.com>
2024-07-29 20:22:37 -0400

    Deleted Skateboarder model and replaced it with Profile.

---
+++ server/models/Profile.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+// We can include goals as a subdocument as they can have mult. goals
+// Within skateboarder schema: define a profile with a name, age, awards, experience or levels
+
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  awards: [{ type: Schema.Types.ObjectId, ref: "awards" }],
+  levels: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  requireSponsorship: {
+    type: Boolean,
+  },
+  bio: {
+    type: Text,
+  },
+  associations: [
+    {
+      type: String,
+    },
+  ],
+  goals: {
+    type: String,
+  },
+});
+// Initialize my Skateboader  model - this might need to go above SkaterSchema
+const Profile = model("profile", profileSchema);
+
+module.exports = Profile;
