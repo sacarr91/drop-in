6990bc6de3ec7717b69667ee610d4d6f8a67760e
Stephanie Carr <sacarr91@gmail.com>
2024-07-30 15:00:28 -0400

    navbar styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,76 @@
-import Image from 'react-bootstrap/Image';
-import NavTabs from "./NavTabs";
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from "react-router-dom";
-import { Nav } from 'react-bootstrap';
 
 import("../utils/header.css")
+import("../utils/fonts.css")
 
 function Header() {
-  return <>
-     <Navbar className="bg-body-tertiary bg-dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src=""
-              className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
-            <NavTabs />
-            </Navbar.Brand>
-        </Container>
-      </Navbar>
+  return (
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary bg-img mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#" className="bangers homeLink">DROP IN</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} className="toggleBtn" />
+            <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end" >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Site Navigation
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <NavDropdown title="Skaters" id={`offcanvasNavbarDropdown-expand-${expand}`} >
+                    <NavDropdown.Item href="#action3">
+                      See All Skaters
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Sponsors" id={`offcanvasNavbarDropdown-expand-${expand}`} >
+                    <NavDropdown.Item href="#action3">
+                      See All Sponsors
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Levels" id={`offcanvasNavbarDropdown-expand-${expand}`} >
+                    <NavDropdown.Item href="#action3">
+                      Levels Description
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#action2">
+                    Other
+                  </Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </>
+  );
 }
 
 export default Header;
