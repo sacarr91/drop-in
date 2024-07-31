4b0eee9bd9992839c995c7c432412d01d86e4b0d
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-30 20:32:33 -0400

    added auth.js and updated server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,45 @@
 require("dotenv").config();
-const express = require("express");
-//  Establishes which port to use and if others aren't available it will use 3001
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
-
-// Establish a connection to our database
-const db = require("../server/config/connection");
-// Create a route path
-const routes = require("./routes");
-
-// Middleware
 const app = express();
-
-// Middleware configuration functionality
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// app.use(routes);
-
-// This is a database event listener that will listen for connections on the specified port
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server is running on port ${PORT}`);
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
---
+++ server/utils/auth.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWTSECRET;
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
