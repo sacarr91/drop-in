06ebcf507bacc0d522d11276682414641b2a43a3
Stephanie Carr <sacarr91@gmail.com>
2024-08-01 19:16:21 -0400

    removed unused imports from NavLinks component

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -1,11 +1,7 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import Auth from '../utils/auth';
 
 
 function NavLinks({ expand }) {
