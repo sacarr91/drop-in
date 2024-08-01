5fe0d0a1b18ff3956ecc9c614fabf471fa5cebcd
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-08-01 12:57:17 -0400

    Integrated header styling with titan framework to meed coding standards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import Header from './components/Header';
 // imports for app styling
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './utils/style.css';
+import './App.css';
 
 // create link to graphQL
 const httpLink = createHttpLink({
