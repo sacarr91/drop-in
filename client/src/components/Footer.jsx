760e6c79fb795641139ccf44fd14fbc48e1ac062
Stephanie Carr <sacarr91@gmail.com>
2024-08-01 19:09:50 -0400

    titan glow icon

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,20 +5,19 @@ import Nav from 'react-bootstrap/Nav';
 
 function Footer() {
   return (
-    <Navbar fixed="bottom" expand="lg" className="bg-body-tertiary">
+    <Navbar fixed="bottom" expand="lg" className="footer-style">
       <Container>
-        <Nav variant="underline" style={{flexDirection: "row"}}>
+        <Nav variant="underline" style={{ flexDirection: "row" }}>
           <Nav.Item>
-            <Nav.Link href="https://github.com/sacarr91" target="_blank"><img src='/images/coderepo.png' height={30}></img></Nav.Link>
+            <Nav.Link href="https://github.com/sacarr91" target="_blank"><img src='client/public/images/coderepo.png' height={30}></img></Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="https://www.linkedin.com/in/stephaniecarr424/" target="_blank"><img src='/images/devteam.png' height={30}></img></Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="https://stackoverflow.com/users/23317680/stephcarr" target="_blank"><img src='/images/stack-logo.png' height={30}></img></Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/stephaniecarr424/" target="_blank"><img src='client/public/images/devteam.png' height={30}></img></Nav.Link>
           </Nav.Item>
         </Nav>
-        <img src='/titan.svg'></img>Titan
+        <Container>
+          <img src='client/public/images/titanglow.png' height={30} style={{ justifySelf: "right" }}></img>Titan
+        </Container>
       </Container>
     </Navbar>
   );
