d2f63feb809828c7da16d9b97d2caece8483b228
Stephanie Carr <sacarr91@gmail.com>
2024-08-01 21:50:00 -0400

    added header & footer to error page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,8 +11,8 @@ import ErrorPage from './pages/ErrorPage';
 
 import AboutSkater from "../src/pages/AboutSkater";
 import AboutSponsor from "../src/pages/AboutSponsor";
-import AllSkaters from './pages/AllSkaters.jsx';
-import AllSponsors from './pages/AllSponsors.jsx';
+import AllSkaters from '../src/pages/AllSkaters.jsx';
+import AllSponsors from '../src/pages/AllSponsors.jsx';
 import Connect from "../src/pages/Connect";
 import SponsorLevels from "../src/pages/SponsorLevels";
 import CreateSkater from "../src/pages/CreateSkater";
