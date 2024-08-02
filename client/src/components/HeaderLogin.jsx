a15dbfe76251167aa50ca7e556831b37f6a77cc2
Stephanie Carr <sacarr91@gmail.com>
2024-08-01 20:27:24 -0400

    update header button styling

--- client/src/components/HeaderLogin.jsx
+++ client/src/components/HeaderLogin.jsx
@@ -10,19 +10,19 @@ function LoginLogic(){
         <>
         {Auth.loggedIn() ? (
           <>
-            <Link to="/" className='me-4 headerbtn'>
+            <Link to="/" className='me-4 btn headerbtn'>
               View My Profile
             </Link>
-            <button onClick={logout} className='me-4 headerbtn'>
+            <button onClick={logout} className='me-4 btn headerbtn'>
               Logout
             </button>
           </>
         ) : (
           <>
-            <Link to="/Login" className='me-4 headerbtn'>
+            <Link to="/Login" className='me-4 btn headerbtn'>
               Login
             </Link>
-            <Link to="/" className='me-4 headerbtn'>
+            <Link to="/" className='me-4 btn headerbtn'>
               Signup
             </Link>
           </>
