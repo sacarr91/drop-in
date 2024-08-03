1a881a72bd50813c08292c3af4e8dbc5d54d739b
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-08-03 08:41:02 -0400

    Add sponsor backend fully functional

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dropin_db');
+mongoose.connect(process.env.MONGOURI || 'mongodb://127.0.0.1:27017/dropin_db');
 
 module.exports = mongoose.connection;
