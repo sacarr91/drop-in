fac9836dab0bd140848be5494ac89c1870a98b53
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-08-03 10:22:06 -0400

    completed seed for render deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dropin_db');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dropin_db');
 
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = mongoose.connection;
