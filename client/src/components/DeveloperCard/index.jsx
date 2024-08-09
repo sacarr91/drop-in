d00163c837d440c39824412e03c674b4a2e75a0d
Stephanie Carr <sacarr91@gmail.com>
2024-08-08 21:06:19 -0400

    site-wide font stylings via css

--- client/src/components/DeveloperCard/index.jsx
+++ client/src/components/DeveloperCard/index.jsx
@@ -1,10 +1,10 @@
 const DeveloperCard = ({ dev }) => (
-    <div style={{ width: '18rem', margin: '1rem', border: '1px solid #ddd', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-        <img src={dev.image} className="card-img-top" alt={dev.name} style={{ borderRadius: '8px 8px 0 0'}} />
-        <div className="card-body" style={{ padding: '1rem' }}>
+    <div className="devCard">
+        <img src={dev.image} className="card-img-top" alt={dev.name} />
+        <div className="card-body">
             <h5 className="card-title text-center">{dev.name}</h5>
             <p className="card-text text-center">{dev.bio}</p>
-            <a href={dev.githubLink} className="btn" style={{ backgroundColor: 'rgb(101, 189, 71)', color: '#fff', padding: '0.5rem 1rem', borderRadius: '4px' }}>{dev.githubName}</a>
+            <a href={dev.githubLink} className="btn gh-link">{dev.githubName}</a>
         </div>
     </div>
 );
