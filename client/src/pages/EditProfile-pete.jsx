cb11617a67e5f7fe8a0a33cf3aa747b923eab33a
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-08-12 09:06:56 -0400

    minor changes - redirecting effort

--- client/src/pages/EditProfile-pete.jsx
+++ client/src/pages/EditProfile-pete.jsx
@@ -1,79 +1,115 @@
-import { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Form, Col } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
 import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { Form, Row, Col } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
-import { EDIT_PROFILE } from '../utils/mutations';
+import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
-const EditProfile = () => {
-    const [formState, setFormState] = useState({
-        age: '',
-        levels: ''
-    });
-
-    const [formGoals, setGoals] = useState([]);
-
-    const [editProfile, { error, data }] = useMutation(EDIT_PROFILE);
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-
-    };
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const { data } = await editProfile({
-                variables: { ...formState },
-            });
-        } catch (e) {
-            console.error(e);
+let isEditProfile = "";
+const UserProfile = () => {
+    const { profileId } = useParams();
+    const { loading, data } = useQuery(
+        profileId ? QUERY_SINGLE_PROFILE : QUERY_ME,
+        {
+            variables: { profileId: profileId },
         }
-    };
+    );
+
+    const profile = data?.me || data?.profile || {};
+
+    if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
+        isEditProfile = true
+        console.log(isEditProfile);
+    }
+
+
+    if (loading) {
+        return <section>Loading....</section>
+    }
+
+    if (!profile?.name) {
+        return (
+            <h4>
+                🤓 You need to be logged in to see profile page. Use the navigation
+                links above to sign up or log in!
+            </h4>
+        )
+    }
 
     return (
-        <Card style={{ width: '50%' }}>
+        <Card style={{width:"50%"}}>
             <Card.Body>
-                <Card.Title>Edit Profile - PETE's CHANGES</Card.Title>
+                <Card.Title>View Profile</Card.Title>
                 <Form>
-                    <Form.Group className="mb-3" controlId="formBasicText">
-                        <Form.Label>
-                            Age
-                        </Form.Label>
-                        <Col>
-                            <Form.Control type="text" value={formState.levels} onChange={handleChange} />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicText">
-                        <Form.Label>
-                            Levels
-                        </Form.Label>
-                        <Col>
-                            <Form.Control type="text" value={formState.levels} onChange={handleChange} />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicText">
-                        <Form.Label>
-                            Goals
-                        </Form.Label>
-                        <Col>
-                            <Form.Control type="text" value={formState.levels} onChange={handleChange} />
-                        </Col>
-                    </Form.Group>
-                    <Link
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+                <Form.Label column sm="2">
+                    Name
+                </Form.Label>
+                    <input value={profile.name}></input>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextAge">
+                <Form.Label column sm="2">
+                    Age
+                </Form.Label>
+                    <input value={profile.age} ></input>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextLevels">
+                <Form.Label column sm="2">
+                    Level
+                </Form.Label>
+                    <input value={profile.levels}></input>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextLevels">
+                <Form.Label column sm="2">
+                    Bio
+                </Form.Label>
+                    <textarea value={profile.bio}></textarea>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextLevels">
+                <Form.Label column sm="2">
+                    email
+                </Form.Label>
+                    <input value={profile.email}></input>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextLevels">
+                <Form.Label column sm="2">
+                    Password
+                </Form.Label>
+                    <input></input>
+            </Form.Group>
+
+
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextGoals">
+                <Form.Label column sm="2">
+                    Goals
+                </Form.Label>
+                    <Form.Control plaintext readOnly defaultValue={profile.goals} />
+            </Form.Group>
+            {isEditProfile && (
+                <Link
                     className="button-link secondary"
-                    to={`/editprofile/`}
-                >Submit</Link>
-                </Form>
+                    to={`/editprofile/${profile._id}`}
+                >Edit Profile</Link>
+            )}
+            {!isEditProfile && (
+                <Link
+                    className="button-link secondary"
+                    to={`/editprofile/${profile._id}`}
+                >Donate</Link>
+            )}
+        </Form>
+        <section>
+            <h1>Requests</h1>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextLevels">
+                    {/* <input value={profile.requests}></input> */}
+            </Form.Group>
+        </section>
             </Card.Body>
         </Card>
     )
-};
 
-export default EditProfile;
+}
+
+
+export default UserProfile;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -35,6 +35,8 @@ export const QUERY_ME = gql`
       age
       levels
       goals
+      bio
+      email
     }
   }
 `;
