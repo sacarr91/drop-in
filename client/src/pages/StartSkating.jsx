074d1c9cfcf17cde1eb9f90bf68b9d7a28dd81bc
Stephanie Carr <sacarr91@gmail.com>
2024-08-12 23:57:12 -0400

    added skaters to seed file

--- client/src/pages/StartSkating.jsx
+++ client/src/pages/StartSkating.jsx
@@ -3,12 +3,37 @@ import Container from 'react-bootstrap/esm/Container';
 function CreateSkater() {
 
     return (
-        <><Container className="py-5">
-            <center><h3>
-            How to get started skating
-            Boards for Bros reference
-            </h3></center>
-        </Container>
+        <>
+            <center>
+                <h1 className='py-3'>
+                    How to Start Skating
+                </h1>
+            </center>
+            <div>
+                <p>
+                    There's tons of advice out there on how to get started skating! Here are a few highlights...
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </p>
+                <h3>More information</h3>
+                <ul>
+                    <a href=''>Tips for Starting Out</a> - 
+                    <br></br>
+                    <a href=''>How to Skateboard: Step-by-Step</a> - TransWorld
+                    <br></br>
+                    <a href=''>A Beginners Guide</a> - Tactics
+                    <br></br>
+                    <a href=''>Learn to Skate Guide</a> - SkateboardGB
+                    <br></br>
+                    <a href=''>Basic Tips for Adults</a> - Ripped Laces
+                </ul>
+            </div>
+            <h2>
+                Boards for Bros reference
+            </h2>
         </>
     )
 };
