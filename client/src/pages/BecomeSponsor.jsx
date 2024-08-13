2a2551432bd92c4f6352f13b8b40fbab2522caf1
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-08-13 12:23:33 -0400

    updated verbiage and eliminated unused nav links

--- client/src/pages/BecomeSponsor.jsx
+++ client/src/pages/BecomeSponsor.jsx
@@ -4,9 +4,33 @@ function Create() {
 
     return (
         <><Container className="py-5">
-            <center><h3>
-            Why Sponsor / How to Sponsor
-            </h3></center>
+            <center>
+            <h3>How to become a sponsor!</h3>
+            <p>Sponsors are traditionally skateboard businesses, merchandisers, and consumer good providers, however with Drop In we aim to extend that to all community supporters!
+                In order to become a sponsor, you simply need to provide a skateboarder with a resource and in return that skateboarder represents you and/or your business. 
+                For many who already are sponsors the intention is to help build their brand awareness through the skateboard community's vast reach with their captivating acrobatics!
+                As social media continues to thrive in the modern era, businesses are able to capitalize on penetrating markets with visibility by supporting strong cultural movements such as skateboarding.
+                We understand there is a mutually beneficial environment where skateboarding attracts viewers and attention with online media and a sponsor's product or logo can be demonstrated. However,
+                being a sponsor might not mean you have a business or desire any recognition, rather an individual may want to sponsor a skateboarder they know to help support their goals. 
+            </p>
+            <p>
+                This is where Drop In comes in, we look to provide and enable opportunities for businesses and individuals who want to support skateboarders. With this platform, friends and family can create profiles and 
+                offer sponsorship for specific goals or whenever they feel like to the skateboarders they are connected with. The concept is based on a natural existing process within the skateboard community,
+                where we frequently get together and have fun. We support each other with food, activities, and very commonly support with equipment or financially. Every little bit helps and with this app we enable friends and supporters
+                to participate in each other's journeys.  
+            </p>
+            <p> To become a sponsor on this platform, simply sign up and select the Sponsor account type under 'Role'. This application is a proof of concept currently with hopes of expanding in the future.
+                As such, sponsoring a skater profile can be done by endorsing the sponsor by clicking the 'Sponsor Me' button on their profile. This will add them as a listed user that you sponsor, and will add you to their profile
+                as their sponsor. You can also offer financial support to a skateboarder profile, by  clicking the 'Donate' button and entering a payment amount and selecting a payment option. 
+            </p>
+            <p>
+                This app is a great way to support your local skateboard community, however sponsoring and getting involved doesn't have to stop here, get out to your local action parks and have some fun! At the local parks you can 
+                learn more about what's going on and other skateboarding events happening nearby. 
+                If you would like to get involved and learn more checkout our friends at Boards for Bros by clicking the link below to learn more about how to get involved with the skateboard community 😀. 
+            </p>
+            <a href='https://boards4bros.org/'><h5>Boards for Bros</h5></a>
+            <img src='https://media.tenor.com/hkd1L4jFJTAAAAAM/skateboarding-dog-viralhog.gif' alt="welcome" className='gif'></img>
+            </center>
         </Container>
         </>
     )
